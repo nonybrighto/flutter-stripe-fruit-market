86f61ea7e13b6c4b16a8e2ac2e05c2c8a349ef1f
Nony Bright <nonybrighto@gmail.com>
2022-07-11 08:49:02 +0100

    Modify functions configuration

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,30 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+exports.createStripeCustomer = functions.firestore
+    .document("customers/{customerId}")
+    .onCreate(async (snap, context) => {
+      try {
+        console.log("Key");
+        console.log(process.env.STRIPE_SECRET_KEY);
+        functions.
+            logger.
+            info("secrete", {secret: process.env.STRIPE_SECRET_KEY});
+        const newValue = snap.data();
+        const customer = await stripe.customers.create({
+          email: newValue.email,
+        });
+        await admin
+            .firestore()
+            .collection("customer")
+            .doc(context.params.customerId)
+            .update({
+              stripeCustomerId: customer.id,
+            });
+      } catch (error) {
+        functions.logger.info("error creating stripe customer", {error: error});
+      }
+    });
