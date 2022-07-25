a8198bb710878d52115fcfa494214e38ad896bb0
Nony Bright <nonybrighto@gmail.com>
2022-07-25 14:29:35 +0100

    Display loading overlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_stripe_payment/pages/auth_page.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'firebase_options.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:flutter_stripe_payment/constants/secrets.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AuthPage(),
+      home: const LoaderOverlay(child: AuthPage()),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter_stripe_payment/services/customer_service.dart';
 import 'package:flutter_stripe_payment/services/payment_service.dart';
 import 'package:flutter_stripe_payment/services/product_service.dart';
 import 'package:flutter_stripe_payment/widgets/product_card.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,6 +20,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Customer? _authenticatedCustomer;
   final CustomerService _customerService = CustomerService();
+  late BuildContext _loadContext;
 
   @override
   void initState() {
@@ -40,36 +42,39 @@ class _HomePageState extends State<HomePage> {
               })
         ],
       ),
-      body: Column(children: [
-        Text(_authenticatedCustomer != null
-            ? _authenticatedCustomer!.email
-            : 'loading..'),
-        FutureBuilder<List<Product>>(
-          future: ProductService().fetchProducts(),
-          initialData: const [],
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Expanded(
-                  child: GridView.builder(
-                itemCount: snapshot.data!.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 8,
-                  mainAxisSpacing: 8,
-                ),
-                itemBuilder: (context, index) {
-                  return ProductCard(
-                    product: snapshot.data![index],
-                    onPurchasePressed: _onPurchasePressed,
-                  );
-                },
-              ));
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
-        ),
-      ]),
+      body: LoaderOverlay(
+        child: Column(children: [
+          Text(_authenticatedCustomer != null
+              ? _authenticatedCustomer!.email
+              : 'loading..'),
+          FutureBuilder<List<Product>>(
+            future: ProductService().fetchProducts(),
+            initialData: const [],
+            builder: (context, snapshot) {
+              _loadContext = context;
+              if (snapshot.hasData) {
+                return Expanded(
+                    child: GridView.builder(
+                  itemCount: snapshot.data!.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                  ),
+                  itemBuilder: (context, index) {
+                    return ProductCard(
+                      product: snapshot.data![index],
+                      onPurchasePressed: _onPurchasePressed,
+                    );
+                  },
+                ));
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+        ]),
+      ),
     );
   }
 
@@ -93,14 +98,17 @@ class _HomePageState extends State<HomePage> {
                   Navigator.of(context).pop();
                   //show payment sheet
                   try {
+                    _loadContext.loaderOverlay.show();
                     final paymentService = PaymentService(
                         authService:
                             AuthService(customerService: CustomerService()));
                     await paymentService.payWithPaymentSheet(
                         productId: product.id);
                   } catch (error) {
+                    _loadContext.loaderOverlay.hide();
                     print(error);
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    ScaffoldMessenger.of(_loadContext)
+                        .showSnackBar(const SnackBar(
                       content: Text("Failed to create payment sheet"),
                     ));
                   }
