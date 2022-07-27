8586d68efcfe64c98c66dfbd763c11a9440c3136
Nony Bright <nonybrighto@gmail.com>
2022-07-27 23:19:30 +0100

    Complete android stripe configuration

--- android/app/src/main/kotlin/com/example/flutter_stripe_payment/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_stripe_payment/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.flutter_stripe_payment
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
